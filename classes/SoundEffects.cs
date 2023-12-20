2e348f8f5531401e6fd52e2e366f9dd7ac394c63
JohnxdLink <JohnxdLink@gmail.com>
2023-12-20 22:06:00 +0800

    Msg: Changing Absolute Path To Relative Path And Adding Error Will Be Save at Errorlog Folder

--- classes/SoundEffects.cs
+++ classes/SoundEffects.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Media;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace JC_Click_V1.classes
@@ -12,7 +10,6 @@ namespace JC_Click_V1.classes
         SoundPlayer soundPlayer = new SoundPlayer();
 
         DateTime lastKeyPressTime = DateTime.MinValue;
-        object lockObject = new object();
 
         public async void PlaySoundEffect(string fileName)
         {
@@ -21,25 +18,33 @@ namespace JC_Click_V1.classes
                 DateTime currentTime = DateTime.Now;
                 TimeSpan elapsedSinceLastKeyPress = currentTime - lastKeyPressTime;
 
-                // Adjust the threshold based on your preference
+                // N: Adjust the threshold based on your preference
                 TimeSpan threshold = TimeSpan.FromMilliseconds(100);
 
                 if (elapsedSinceLastKeyPress >= threshold)
                 {
-                    string audioFilePath = $"C:\\Users\\John Christian\\Documents\\Visual Studio\\C# Development\\JC-Click-V1\\resources\\audio\\{fileName}";
+                    // N: Get the directory where the application is running
+                    string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
 
-                    // Use async/await to play the sound asynchronously
+                    // N: Navigate up to the project root from the "classes" folder
+                    string projectRoot = Path.GetFullPath(Path.Combine(baseDirectory, "..", ".."));
+
+                    // N: Construct the relative path to the audio file
+                    string audioFilePath = Path.Combine(projectRoot, "resources", "audio", fileName);
+
+                    // N: Use async/await to play the sound asynchronously
                     await Task.Run(() =>
                     {
                         try
                         {
                             soundPlayer.SoundLocation = audioFilePath;
-                            soundPlayer.Load(); // Ensure the sound is loaded before playing
+                            soundPlayer.Load(); // N: Ensure the sound is loaded before playing
                             soundPlayer.Play();
                         }
                         catch (Exception ex)
                         {
-                            Console.WriteLine($"Error playing sound effect: {ex.Message}");
+                            // N: Log the error to a text file in the "resources/errorlogs" folder
+                            LogError(ex, projectRoot);
                         }
                     });
 
@@ -48,7 +53,34 @@ namespace JC_Click_V1.classes
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error playing sound effect: {ex.Message}");
+                // N: Log the error to a text file in the "resources/errorlogs" folder
+                LogError(ex, AppDomain.CurrentDomain.BaseDirectory);
+            }
+        }
+
+        private void LogError(Exception ex, string projectRoot)
+        {
+            try
+            {
+                // N: Create the "errorlogs" folder if it doesn't exist
+                string errorLogsFolder = Path.Combine(projectRoot, "resources", "errorlogs");
+                Directory.CreateDirectory(errorLogsFolder);
+
+                // N: Create a unique filename based on the current timestamp and the hash code of the exception
+                string timestamp = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string hashCode = ex.GetHashCode().ToString("X");
+                string errorLogFileName = $"errorlog_{timestamp}_{hashCode}.txt";
+
+                // N: Construct the full path to the error log file
+                string errorLogFilePath = Path.Combine(errorLogsFolder, errorLogFileName);
+
+                // N: Write the error details to the error log file
+                File.WriteAllText(errorLogFilePath, $"Timestamp: {timestamp}\nError: {ex.ToString()}");
+            }
+            catch (Exception logEx)
+            {
+                // N: If an error occurs while trying to log, output to the console
+                Console.WriteLine($"Error logging to error log file: {logEx.Message}");
             }
         }
     }
